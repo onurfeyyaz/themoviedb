ef0039c59e3f744fc723b30cd2c39ce7710cfaec
Feyyaz ONUR <feyyazonur0@gmail.com>
2024-09-01 19:21:03 +0300

    Initial Commit

---
+++ TMDB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TMDB
+//
+//  Created by Feyyaz ONUR on 1.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TMDBTests/TMDBTests.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBTests.swift
+//  TMDBTests
+//
+//  Created by Feyyaz ONUR on 1.09.2024.
+//
+
+import Testing
+@testable import TMDB
+
+struct TMDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
